d15c3ca02064cc6181855de193daa657a09eafa2
Luke <lucian.io@outlook.com>
2023-10-25 05:21:20 -0400

    fetching recipes formatted

--- src/gpt_3_5_turbo.rs
+++ src/gpt_3_5_turbo.rs
@@ -3,21 +3,17 @@ use dotenv::dotenv;
 use std::env;
 use chatgpt::prelude::*;
 
+use crate::recipe::get_recipe;
+
 pub async fn chat(ingredients: Vec<&str>) -> Result<()> {
     dotenv().ok();
     let api_key = env::var("API_KEY").expect("API_KEY not found.");
     let client = ChatGPT::new(api_key)?;
+    let content: String =
+        "Complete the following recipe with minimal scientific fashion, ingredients and instructions labeled.".to_owned() +
+        &ingredients.join(", ");
 
-    let initial_message =
-        "You are a master chef who specializes in simplicity of meals around the world for the average person.";
-    let content: String = ingredients.join(", ");
-    let combined_message = format!(
-        "{} Build me a recipe with the following ingredients, {}.",
-        initial_message,
-        content
-    );
-
-    let response = client.send_message(combined_message).await?;
-    println!("Response: {}", response.message().content);
+    let response = client.send_message(content.clone()).await?;
+    get_recipe(&response);
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 mod gpt_3_5_turbo;
+mod recipe;
 use gpt_3_5_turbo::chat;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let ingredients = vec!["cheese", "bread", "butter"];
+    let ingredients = vec!["ramen", "peanut butter", "olive oil", "cheese"];
     chat(ingredients).await?;
     Ok(())
 }
