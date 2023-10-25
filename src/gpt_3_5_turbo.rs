f5edc1cfc24e746f199c99a4df3365668498a733
Luke <lucian.io@outlook.com>
2023-10-25 00:10:46 -0400

    ingredients

--- src/gpt_3_5_turbo.rs
+++ src/gpt_3_5_turbo.rs
@@ -3,25 +3,27 @@ use reqwest;
 use serde_json::json;
 use std::env;
 
-pub async fn chat(role: &str, content: &str) -> Result<(), reqwest::Error> {
+pub async fn chat(role: &str, ingredients: Vec<&str>) -> Result<(), reqwest::Error> {
     dotenv().ok();
     let client = reqwest::Client::new();
     let api_key = env::var("API_KEY").expect("API_KEY not found.");
+    let content = ingredients.join(", ");
     let request_data =
         json!({
         "model": "gpt-3.5-turbo",
         "messages": [
             {
                 "role": "system",
-                "content": "You are clear and concise concentrated and meditated, do not respond with code."
+                "content": "You are a master chef who specializes in simplicity of meals around the world for the average person."
             },
             {
                 "role": role.to_string(),
-                "content": content.to_string()
+                "content": format!("Build me a recipe with the following ingredients, {}.", content)
             }
         ],
         "temperature": 0.7
     });
+
     let response = client
         .post("https://api.openai.com/v1/chat/completions")
         .header("Authorization", format!("Bearer {}", api_key))
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,8 @@ use gpt_3_5_turbo::chat;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    const ROLE: &str = "user";
-    const CONTENT: &str =
-        "what are some examples of programs I can write to utilize chatGPT api with a rust back";
-    chat(ROLE, CONTENT).await?;
+    let role = "user";
+    let ingredients = vec!["cheese", "bread", "butter"];
+    chat(role, ingredients).await?;
     Ok(())
 }
