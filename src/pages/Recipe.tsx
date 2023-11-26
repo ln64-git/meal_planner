b6b89fa1e619e29c9a15381ae78ca1c0d54888f3
Luke <lucian.io@outlook.com>
2023-11-26 05:49:52 -0500

    installed prettier-plugin-tailwind

--- src/pages/Recipe.tsx
+++ src/pages/Recipe.tsx
@@ -1,75 +1,75 @@
-import {useState} from "react"
-import {invoke} from "@tauri-apps/api/tauri"
-import {sampleRecipe} from '../assets/sampleRecipe.ts'
+import { useState } from 'react';
+import { invoke } from '@tauri-apps/api/tauri';
+import { sampleRecipe } from '../assets/sampleRecipe.ts';
 
 interface Recipe {
-  name: string
-  ingredients: string[]
-  instructions: string[]
-  note?: string
+  name: string;
+  ingredients: string[];
+  instructions: string[];
+  note?: string;
 }
 
 function App() {
-  const [ingredients, setIngredients] = useState("")
-  const [response, setResponse] = useState<Recipe | string>("")
+  const [ingredients, setIngredients] = useState('');
+  const [response, setResponse] = useState<Recipe | string>('');
 
   const getRecipe = async () => {
     const ingredientsArray = ingredients
-      .split(",")
-      .map((ingredient) => ingredient.trim())
-    const recipeResponse = await invoke("get_recipe", {
+      .split(',')
+      .map((ingredient) => ingredient.trim());
+    const recipeResponse = await invoke('get_recipe', {
       ingredients: ingredientsArray,
-    })
-    console.log(recipeResponse)
-    setResponse(recipeResponse as Recipe)
-  }
+    });
+    console.log(recipeResponse);
+    setResponse(recipeResponse as Recipe);
+  };
 
   return (
-    <div className='flex flex-col items-center justify-center h-screen bg-stone-800'>
+    <div className="flex h-screen flex-col items-center justify-center bg-stone-800">
       <form
-        className='flex flex-row items-center w-full absolute top-0'
+        className="absolute top-0 flex w-full flex-row items-center"
         onSubmit={async (e) => {
-          e.preventDefault()
-          await getRecipe()
+          e.preventDefault();
+          await getRecipe();
         }}
       >
         <input
           onChange={(e) => setIngredients(e.currentTarget.value)}
-          className='flex flex-grow'
-          placeholder='Enter a name...'
+          className="flex flex-grow"
+          placeholder="Enter a name..."
         />
-        <button type='submit'>Submit</button>
+        <button type="submit">Submit</button>
       </form>
 
-        <div className='flex flex-col items-center bg-stone-900 text-white max-w-2xl mx-auto w-full overflow-auto p-6'>
-          <div className='text-3xl text-center pb-4'>{sampleRecipe.name}</div>
-          <div className='flex '>
-            <div className='flex flex-col  pl-8'>
-              <h3 className='font-bold text-sm'>Instructions:</h3>
-              <ul>
-                {sampleRecipe.ingredients.map((ingredient, index) => (
-                  <div className='flex' key={index}>
-                    <p className='pr-2'>{"\u2022"}</p>
-                    <li>{ingredient}</li>
-                  </div>
-                ))}
-              </ul>
-            </div>
-            <div className='flex flex-col  px-8'>
-              <h3 className='font-bold text-center text-sm'>Instructions:</h3>
-              <ol>
-                {sampleRecipe.instructions.map((instruction, index) => (
-                  <div className='flex break-words' key={index}>
-                    <p className='pr-2'>{index + 1}.</p>
-                    <li>{instruction}</li>
-                  </div>
-                ))}
-              </ol>
-            </div>
+      <div className="mx-auto flex w-full max-w-2xl flex-col items-center overflow-auto bg-stone-900 p-6 text-white">
+        <div className="pb-4 text-center text-3xl">{sampleRecipe.name}</div>
+        <div className="flex">
+          <div className="flex flex-col pl-8 ">
+            <h3 className="text-sm font-bold">Instructions:</h3>
+            <ul>
+              {sampleRecipe.ingredients.map((ingredient, index) => (
+                <div className="flex" key={index}>
+                  <p className="pr-2">{'\u2022'}</p>
+                  <li>{ingredient}</li>
+                </div>
+              ))}
+            </ul>
+          </div>
+          <div className="flex flex-col px-8">
+            <h3 className="text-center text-sm font-bold">Instructions:</h3>
+            <ol>
+              {sampleRecipe.instructions.map((instruction, index) => (
+                <div className="flex break-words" key={index}>
+                  <p className="pr-2">{index + 1}.</p>
+                  <li>{instruction}</li>
+                </div>
+              ))}
+            </ol>
           </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
